a00ec495d8b99e686e8b2686941efa1295490d2c
OmarxEmara <80140756+OmarxEmara@users.noreply.github.com>
2025-11-11 09:47:28 +0200

    Implement Course class with student management
    Added Course class with attributes and methods.

--- Course.h
+++ Course.h
@@ -4,19 +4,23 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+
+    class Course {
+        private:
+            string courseCode;
+            string courseName;
+            int maxStudents;
+            Student* students;
+            int currentStudents;
+        public:
+            Course(){};
+            Course(string courseCode, string courseName, int maxStudents, Student *students, int currentStudents);
+            ~Course();
+            void addStudent(const Student& s);
+            void displayCourseInfo();
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+    };
 #endif
