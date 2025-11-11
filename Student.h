86a02d1f8115f9f863f2a7cda16d27ee926f150b
OmarxEmara <80140756+OmarxEmara@users.noreply.github.com>
2025-11-11 09:49:23 +0200

    Update Student class definition and attributes
    Refactored Student class to include year level and major attributes.

--- Student.h
+++ Student.h
@@ -1,21 +1,22 @@
+#include "Person.h"
 #ifndef STUDENT_H
 #define STUDENT_H
+using namespace std;
 
-#include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
+class Student : public Person {
+    private:
+        int yearlevel;
+        string major;
+    public:
+        Student(){};
+        Student(string name, int id, int yearlevel,string major);
+        ~Student(){};
+        void display();
+        void setYearLevel(int yearlevel);
+        void setMajor(string major);
+        int getYearLevel(){};
+        string getMajor(){};
 
-
-
-
-
-
-
-
-
-
-
-
+};
 
 #endif
